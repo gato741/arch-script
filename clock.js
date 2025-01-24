544e0e3ec149c68a5b7b84bcd0fc63eec1dab76b
gato741 <maksymiliancapala1@gmail.com>
2025-01-24 16:20:20 +0100

    24.01.2024 16:20

--- clock.js
+++ clock.js
@@ -1,2 +1,13 @@
-/* Last updated: xx.xx.xxxx, xx:xx
+/* Last updated: 24.01.2024, 16:20
    Report bugs if there are any */
+
+let time = new Date();
+
+let hours = time.getHours();
+let minutes = time.getMinutes();
+
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}  
+
+console.log(hours + ":" + minutes);
